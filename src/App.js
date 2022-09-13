924359bf30d94047afb0cbcabd1ad61272568f9c
samandar-dev <samandarbohodirov01@gmail.com>
2022-09-13 13:50:21 +0500

    add

--- src/App.js
+++ src/App.js
@@ -166,7 +166,7 @@ function App() {
           <div className='app__postRight-top'>
             <Avatar className='post__avatar' alt="Remy Sharp" src="https://thumbs.dreamstime.com/b/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80-%D1%81%D0%BC%D0%B8-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B0-%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8F-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F-%D0%BF%D0%BE-%D1%83%D0%BC%D0%BE%D0%BB%D1%87%D0%B0%D0%BD%D0%B8%D1%8E-%D1%81%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80-176194876.jpg" />
 
-            <h3>{username}</h3>
+            <h3>john</h3>
           </div>
 
           <div className='app__postRight-img'>
